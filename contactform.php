8e07251593ba8d406a78e168d55d106585dfd466
soumyajit4419 <ashish454570@gamil.com>
2019-07-06 22:13:18 +0530

    added php form

--- contactform.php
+++ contactform.php
@@ -1,12 +1,13 @@
 <?PHP
+
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $phone=$_POST['phone'];
 $message = $_POST['message'];
 
-$email_from = 'sbjit.co.nf';
+$email_from = 'ashish454570@gmail.com';
 $email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name.\n"."phone number $phone.\n"
+$email_body = "You have received a new message from the user $name.\n". "phone number $phone.\n".
 "Here is the message:\n $message".
 
 
@@ -14,7 +15,4 @@ $to = "ashish454570@gmail.com";
  $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
-
-
-
 ?>
