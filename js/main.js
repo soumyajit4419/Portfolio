2e1320bdb3797e78a516bcfe6ae9d456dea918ea
soumyajit4419 <ashish454570@gmail.com>
2020-04-27 08:32:38 +0530

    upadte in cetificate gallery style

--- js/main.js
+++ js/main.js
@@ -173,6 +173,42 @@ AOS.init({
 		}
 	});
 
-
 })(jQuery);
+
+
+	$('.imageGallery1 .light').simpleLightbox();
+
+	function gallery_isotope() {
+		if ($('.gallery_f_inner').length) {
+			// Activate isotope in container
+			$(".gallery_f_inner").imagesLoaded(function () {
+				$(".gallery_f_inner").isotope({
+					layoutMode: 'fitRows',
+					animationOptions: {
+						duration: 750,
+						easing: 'linear'
+					}
+				});
+			});
+
+			// Add isotope click function
+			$(".gallery_filter li").on('click', function () {
+				$(".gallery_filter li").removeClass("active");
+				$(this).addClass("active");
+
+				var selector = $(this).attr("data-filter");
+				$(".gallery_f_inner").isotope({
+					filter: selector,
+					animationOptions: {
+						duration: 450,
+						easing: "linear",
+						queue: false,
+					}
+				});
+				return false;
+			});
+		}
+	}
+	gallery_isotope();
+
 
