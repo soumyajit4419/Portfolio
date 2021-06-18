208ac6e1b0f92ed268e711dbe146332b55e3a364
Matteo Bruni <ar3s@icloud.com>
2021-06-19 01:19:12 +0200

    replaced react-particles-js with the updated react-tsparticles

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Particles from "react-particles-js";
+import Particles from "react-tsparticles";
 
 function Particle() {
   return (
@@ -10,10 +10,10 @@ function Particle() {
             value: 160,
             density: {
               enable: true,
-              value_area: 1500,
+              area: 1500,
             },
           },
-          line_linked: {
+          links: {
             enable: false,
             opacity: 0.03,
           },
@@ -25,27 +25,27 @@ function Particle() {
             value: 1,
           },
           opacity: {
-            anim: {
+            animation: {
               enable: true,
               speed: 1,
-              opacity_min: 0.05,
+              minimumValue: 0.05,
             },
           },
         },
         interactivity: {
           events: {
-            onclick: {
+            onClick: {
               enable: true,
               mode: "push",
             },
           },
           modes: {
             push: {
-              particles_nb: 1,
+              quantity: 1,
             },
           },
         },
-        retina_detect: true,
+        detectRetina: true,
       }}
     />
   );
