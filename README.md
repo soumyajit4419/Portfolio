95a5df488c8f688986b7e82f94857ee5df2ecc82
soumyajit4419 <ashish454570@gmail.com>
2021-07-16 13:21:31 +0530

    fixed emoji error

--- README.md
+++ README.md
@@ -72,3 +72,5 @@ You will find all the components used and you can edit your information accordin
 ### Show your support
 
 Give a ⭐ if you like this website!
+
+<a href="https://www.buymeacoffee.com/soumyajit4419" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-violet.png" alt="Buy Me A Coffee" height= "60px" width= "217px" ></a>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There! <span className="wave">👋🏻</span>
+                Hi There! <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
               </h1>
 
               <h1 className="heading-name">
