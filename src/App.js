760c5c761dc13c42bb16a17a20b2d3dd4154cf2b
soumyajit4419 <ashish454570@gmail.com>
2021-10-15 23:53:51 +0530

    feature(added new resume page)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/Resume";
+import Resume from "./components/Resume/ResumeNew";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 import "./App.css";
