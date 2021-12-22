f47f53de26b175d335d5a256bb36dcdaac3c93f1
bryanbeltran <bryan.beltran@mnsu.edu>
2021-12-21 18:30:39 -0600

    Update Footer.js
    add copyright column

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,6 +13,9 @@ function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year}</h3>
+        </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
