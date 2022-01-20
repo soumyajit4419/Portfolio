525ad4e353909060ba38b35e8b2679a31f47015b
Jashandeep Singh <j268sing@edu.uwaterloo.ca>
2022-01-20 00:59:45 -0500

    router refresh

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter basename="/" >
+    <BrowserRouter basename="/portfolio" >
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
