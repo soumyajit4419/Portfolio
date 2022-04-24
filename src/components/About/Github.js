92ee51822b22ca55bae00a8063ded2735471163a
niansahc <97317555+niansahc@users.noreply.github.com>
2022-04-24 14:21:55 -0400

    formatted github calendar

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,13 +6,13 @@ function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        Days I <strong className="purple">Code</strong>
+        WATCH ME <strong className="orange">GROW</strong>
       </h1>
       <GitHubCalendar
-        username="soumyajit4419"
+        username="niansahc"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#c34c2d"
         fontSize={16}
       />
     </Row>
