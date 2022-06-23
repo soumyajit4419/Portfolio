c4e83de2717b1038cbb4c97b1c3a13715bc81f20
Hiten1708 <68720970+Hiten1708@users.noreply.github.com>
2022-06-22 22:28:32 -0400

    Fixed chatify link

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="chatify.soumya-jit.tech/"
+              demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
