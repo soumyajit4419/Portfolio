3f14c397495b292b405fc57363c66b9b95e1d26c
soumyajit4419 <ashish454570@gmail.com>
2022-06-26 10:08:14 +0530

    updated ui images

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { BiLinkExternal } from "react-icons/bi";
+import { CgWebsite } from "react-icons/cg";
+import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
@@ -12,22 +13,26 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-          <Button variant="primary" href={props.ghLink} target="_blank">
-            <BiLinkExternal /> &nbsp;
-            {props.isBlog ? "View Blog" : "View GitHub"}
+        <Button variant="primary" href={props.ghLink} target="_blank">
+          <BsGithub /> &nbsp;
+          {props.isBlog ? "Blog" : "GitHub"}
+        </Button>
+        {"\n"}
+        {"\n"}
+
+        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+
+        {!props.isBlog && props.demoLink && (
+          <Button
+            variant="primary"
+            href={props.demoLink}
+            target="_blank"
+            style={{ marginLeft: "10px" }}
+          >
+            <CgWebsite /> &nbsp;
+            {"Demo"}
           </Button>
-          {'\n'}
-          {'\n'}
-
-          {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-          {!props.isBlog && props.demoLink &&
-              <Button variant="primary" href={props.demoLink} target="_blank">
-                <BiLinkExternal /> &nbsp;
-                {"View Demo"}
-              </Button>  
-            }
-          
+        )}
       </Card.Body>
     </Card>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
+import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
