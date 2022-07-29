9662e9d9983dd2d0e9cd4bac34fef3c13c8496ae
sarkarghya <arghya@nyu.edu>
2022-07-29 12:55:35 -0400

    basic build

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
+            Effective Altruism is going good <span className="purple">Better </span>
             from <span className="purple"> Bhubaneswar, India.</span>
             <br />I am a junior pursuing IMSC in Maths and Computing in BIT
             Mesra.
