03fa411e42cb685a8756bb1e14ebd6979e2ff0cc
SohaibMan <souhaibemanah@gmail.com>
2022-10-21 20:28:47 +0100

    align projects cards buttons

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -10,9 +10,11 @@ function ProjectCards(props) {
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
+        <Card.Text style={{ textAlign: "justify",height:"15rem" ,color:"#adb5bd"}}>
           {props.description}
         </Card.Text>
+        <Card.Footer>
+
         <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
@@ -33,6 +35,7 @@ function ProjectCards(props) {
             {"Demo"}
           </Button>
         )}
+        </Card.Footer>
       </Card.Body>
     </Card>
   );
