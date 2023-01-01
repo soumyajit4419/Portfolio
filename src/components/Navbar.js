46c2122411bbb76c5a58377e5c9dcf7864c5a9f6
Soumyajit Behera <soumyajit@192.168.1.4>
2023-01-01 22:38:44 +0530

    added new resume and fixed navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://blogs.soumya-jit.tech/"
+                href="https://soumyajitblogs.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
