0a63f238a7585a5fe77d16824aa5ec7c5928e4dd
Barcode <40361000+jerryisuwamakeri@users.noreply.github.com>
2023-02-11 03:10:40 +0100

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Makeri Jerry Isuwa </span>
+            from <span className="purple"> Sanga LGA Kaduna State, Nigeria.</span>
+            <br />I am a Full Stack Programmer, Agronomist and a Hip-Hop Head.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from coding, some other activities that I love to do includes;
           </p>
           <ul>
             <li className="about-activity">
@@ -24,14 +23,14 @@ function AboutCard() {
               <ImPointRight /> Writting Tech Blogs
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Travelling to new places
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "In life, Strive to improve!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Makeri</footer>
         </blockquote>
       </Card.Body>
     </Card>
