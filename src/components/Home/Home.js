bec63716665b2ae20cff8e822e63fc7e65856152
Barcode <40361000+jerryisuwamakeri@users.noreply.github.com>
2023-02-11 03:35:18 +0100

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Makeri Jerry isuwa Portfolio</strong>
+                <strong className="main-name"> Makeri Jerry isuwa</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
