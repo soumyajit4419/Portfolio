8d45bf499cc46939ac3e3d62ecb274bff1e33168
Barcode <40361000+jerryisuwamakeri@users.noreply.github.com>
2023-02-14 23:52:47 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,9 +26,7 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="ChatLog"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages.
             />
           </Col>
 
