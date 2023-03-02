c51d0bb6a70539cf5323022a4a6ceb3b194c1fa3
jasper_a <andrea.nicole.jasper@gmail.com>
2023-03-02 14:02:52 -0700

    updates content for projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
@@ -26,9 +23,9 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Job Tracker"
-              description="Personal job application to track my job hunt activities and status of applications. Built using Rails and Tailwind CSS."
+              description="Personal project to track my job hunt activities and status of each applications. It kees me on track for follow ups and where I'm at in the interview process. <br><b>Stack:</b> Linux, Rails and Tailwind CSS."
               ghLink="https://github.com/AndreaJasper/job-tracker"
-              demoLink="https://chatify-49.web.app/"
+              // demoLink="https://chatify-49.web.app/"
             />
           </Col>
 
@@ -37,9 +34,9 @@ function Projects() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="Daily Reads"
-              description="I wanted to play around with site scrapping and pull data from my favorites sites into one location. "
+              description="I have several interests and I'm looking for updates on a daily basis. Instead of going to individual sites, I wanted to play around with site scrapping to pull data from my favorites sites into one location. Built using Node.js and Puppeteer."
               ghLink="https://github.com/AndreaJasper/daily-reads"
-              demoLink="https://blogs.soumya-jit.tech/"
+              // demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
@@ -48,43 +45,9 @@ function Projects() {
               imgPath={editor}
               isBlog={false}
               title="DnD Character Archive"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description="I love playing Dungeons and Dragon, the problem is, I end up with alot of lose paper for each character. While I could use online sources like DnD Beyond, I wanted to practice my coding skills, so I built my own character archive so I can ditch the paper. Built using Rails and Bootstrap."
               ghLink="https://github.com/AndreaJasper/dnd-character-tracker"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Work Order Management"
-              description="Online work order management app that allows users to create and track workorders from start to finish. Built with Sinatra, Bcrypt, Sqlite3 and Bootstrap"
-              ghLink="https://github.com/AndreaJasper/Sinatra-Work-Order-App"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Color Game"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Fwitter"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              // demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
         </Row>
