7287e0e51d1656ec7cc1d3f9f00cc3bbb7c80b16
Sohaib Manah <souhaibemanah@gmail.com>
2023-03-08 22:21:57 +0100

    fix navbar logo link page refrech issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,9 +38,9 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
+        <Link to="/" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
+        </Link>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
