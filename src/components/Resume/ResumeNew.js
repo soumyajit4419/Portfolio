ad5d2e03e2e2b6386229c83d6200afc4b90173a4
jasper_a <andrea.nicole.jasper@gmail.com>
2023-03-31 17:56:13 -0600

    updates to resume component

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -6,6 +6,7 @@ import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import TimeLine from "./TimeLine";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
@@ -34,11 +35,13 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
+        {/* <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-        </Row>
+        </Row> */}
+
+        <TimeLine />
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
---
+++ src/components/Resume/TimeLine.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+function TimeLine() {
+  return (
+    <Container id="cd-timeline" className="cd-container">
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture">
+            </div>
+            <div class="cd-timeline-content">
+                <h2>Freelance</h2>
+                <div class="timeline-content-info">
+                    <span class="timeline-content-info-title">
+                        <i class="fa fa-certificate" aria-hidden="true"></i>
+                        Software Developer
+                    </span>
+                    <span class="timeline-content-info-date">
+                        <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                        February 2023 - Present
+                    </span>
+                </div>
+                <p>Worked alongside lead developer on green field project, architected the database, and implemented new features.</p>
+                <ul class="content-skills">
+                    <li>Rails</li>
+                    <li>React</li>
+                    <li>Docker</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture">
+            </div>
+            <div class="cd-timeline-content">
+                <h2>Switchbox Inc</h2>
+                <span class="timeline-content-info-title">
+                    Software Developer
+                </span>
+                <p>Worked alongside lead developer on green field project, architected the database, and implemented new features.</p>
+                <ul class="content-skills">
+                    <li>Rails</li>
+                    <li>React</li>
+                    <li>Docker</li>
+                </ul>
+                <span class="cd-date">2022</span>
+            </div>
+        </div>
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture">
+            </div>
+            <div class="cd-timeline-content">
+                <h2>Project Management Professional</h2>
+                <p>Earned my PMP through the Project Management Institute.</p>
+                <span class="cd-date">2022</span>
+            </div>
+        </div>
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture">
+            </div>
+            <div class="cd-timeline-content">
+                <h2>Radial Development Group</h2>
+                <span class="timeline-content-info-title">
+                    Software Developer
+                </span>
+                <p>Lead a team of developer that implemented new features, updated out-of-date codebases, and integrated React with Rails.</p>
+                <ul class="content-skills">
+                    <li>Rails</li>
+                    <li>React</li>
+                    <li>SASS</li>
+                    <li>TDD</li>
+                    <li>Agile</li>
+                </ul>
+                <span class="cd-date">2021</span>
+            </div>
+        </div>
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture">
+            </div>
+            <div class="cd-timeline-content">
+                <h2>Flatiron</h2>
+                <span class="timeline-content-info-title">
+                    Software Developer Student
+                </span>
+                <p>Built full-stack projects in Ruby on Rails, exposed to React, Redux, and Node.</p>
+                <ul class="content-skills">
+                    <li>Ruby</li>
+                    <li>Rails</li>
+                    <li>SQLite</li>
+                    <li>TDD</li>
+                </ul>
+                <span class="cd-date">2020</span>
+            </div>
+        </div>
+
+        <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture">
+            </div>
+            <div class="cd-timeline-content">
+                <h2>Madwire</h2>
+                <span class="timeline-content-info-title">
+                    Project Manager/Web Developer
+                </span>
+                <p>Built full-stack projects in Ruby on Rails, exposed to React, Redux, and Node.</p>
+                <ul class="content-skills">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>WordPress</li>
+                    <li>Project Management</li>
+                </ul>
+                <span class="cd-date">2012</span>
+            </div>
+        </div>
+    </Container>
+
+  );
+}
+
+export default TimeLine;
