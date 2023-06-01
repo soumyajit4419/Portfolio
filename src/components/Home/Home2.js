122157901dca99b22fde24042940dbe8a71ba5a2
jasper_a <andrea.nicole.jasper@gmail.com>
2023-06-01 15:03:49 -0600

    content update

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -11,14 +11,14 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
   return (
-    <Container fluid className="home-about-section" id="about">
+    <Container fluid className="home-about-section">
       <Container>
         <Row>
           <h1 style={{ fontSize: "2.6em" }}>
             About me
           </h1>
           <Col md={10} className="home-about-body">
-              During my tenure as a project manager at a tech company, I discovered my profound passion for programming. This love stemmed from my exposure to various projects and technologies. I have acquired fluency in both Rails and Javascript, allowing me to effectively navigate these programming languages. I'm continuously expanding my skills and knowledge in these areas is a priority for me, as I strive to stay abreast of the latest trends and best practices in the world of programming.
+              During my tenure as a project manager at a tech company, I discovered my profound passion for programming. This love stemmed from my exposure to various projects and technologies. I have acquired fluency in both Rails and Javascript, allowing me to effectively navigate these programming languages. Continuously expanding my skills and knowledge in these areas is a priority for me, as I strive to stay abreast of the latest trends and best practices in the world of programming.
           </Col>
         </Row>
       </Container>
