858982226b271e26ddc8034207474641fab61a1d
Saahil <saahilattud@gmail.com>
2023-06-12 19:12:08 +0000

    Update git img

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -8,13 +8,15 @@ function Github() {
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
-      <GitHubCalendar
+      {/* <GitHubCalendar
         username={config.github_username}
         blockSize={15}
         blockMargin={5}
         color="#c084f5"
         fontSize={16}
-      />
+      /> */}
+      {/* svg from git, updated every 3 hours */}
+      <img src="https://raw.githubusercontent.com/NeonGamerBot-QK/NeonGamerBot-QK/output/github-contribution-grid-snake.svg" />
     </Row>
   );
 }
