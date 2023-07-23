74b98a0084961be80091a89305bcc475ed2228d0
ngtuthanhan <ngtuthanhan@gmail.com>
2023-07-23 17:32:01 +0700

    Update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -34,8 +34,10 @@ function ResumeNew() {
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
+
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
