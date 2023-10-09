d4969ca01cde4e3a4751c8bb59876ae769e02262
Gurvinder Dhillon <108829574+gurvinder-dhillon@users.noreply.github.com>
2023-10-09 15:45:24 -0700

    analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga4";
+const TRACKING_ID = "G-X4LH9QYS8Q";
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
