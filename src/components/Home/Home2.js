9c22f5318af7a3540145fdaa1101029ebe3014a9
Dele Paago <delebliss42@gmail.com>
2023-10-25 17:51:32 +0000

    Editing Home2

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -23,18 +23,18 @@ function Home2() {
             with an unwavering passion for environmental innovation. A chess-playing, pan-Africanist wordsmith, 
             my journey blends technology, politics, and spirituality on a quest to inspire and impact the world.
               <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
+              <br />
+              I am fluent in classics like<i>
+                <b className="purple"> C++, Javascript and Go. </b> I speak <b className="purple"> English and German </b>
+              </i> 
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My field of interests include &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple">Web Technologies and Products </b> as well 
+                {" "}
                 <b className="purple">
-                  Blockchain.
+                  Environmental Advocacy
                 </b>
               </i>
               <br />
@@ -68,7 +68,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/captainpaago"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -78,7 +78,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/dbpaago"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -88,7 +88,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/dbpaago42/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -98,7 +98,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/delepaago"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
