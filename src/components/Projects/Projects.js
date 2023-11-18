d756c667dbe996282fb4b18667ebf4eb667b6b56
Zach Antunes <ztunes4@gmail.com>
2023-11-17 17:07:57 -0800

    added projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import horseCreator from "../../Assets/Projects/horse_creator.png";
+import artMuseum from "../../Assets/Projects/art_museum.png";
+import blogWebsite from "../../Assets/Projects/blog_website.png";
+import horseBattle from "../../Assets/Projects/horse_battle.png";
+import noteTaker from "../../Assets/Projects/note_taker.png";
+import weatherApp from "../../Assets/Projects/weather_app.png";
 
 function Projects() {
   return (
@@ -23,68 +23,67 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={artMuseum}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="ArtHub"
+              description="Online art museum that allows users to view historic art pieces and learn the background history of their favorite artists. Collaborated with teammates in UCB coding bootcamp to build."
+              ghLink="https://github.com/t2na/project_ArtHub"
+              demoLink="https://museum-gallery-d60bff79cfff.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={blogWebsite}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Blog Website"
+              description="A fantasy football blog page built following the MVC paradigm. Users can publish their blog posts and comment on others' blog posts. Built with JavaScript, CSS, and Handlebars."
+              ghLink="https://github.com/t2na/blog-website"
+              demoLink="https://fantasy-blog-post-website-d658dedf5ab3.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={horseBattle}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Horse Battle"
+              description="An online game that leverages a backend 'horse API' to randomly generate the names and race tracks for a given horse. The JavaScript logic I wrote then randomly generates horse stats which are used to have the horses 'race' against each other and declare a victor."
+              ghLink="https://github.com/t2na/BerkeleyRacingClub"
+              demoLink="https://tannerrhines.github.io/BerkeleyRacingClub/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={horseCreator}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Horse Creator"
+              description="Another horse-themed application. This time, one that allows users to log in to create their own horse. They're able to give the horse a name, breed and choose what stats to give them, then have the horse saved to the home page. "
+              ghLink="https://github.com/t2na/my-horse-creator"
+              demoLink="https://horse-creator-app-9839a5a7a948.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={noteTaker}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Note Taker"
+              description="A note taking application that I wrote the backend for. The application allows you to save notes to a database, as well as delete them. THe project was built using express.js for the backend routes that facilitate the creating, saving, and deleting."
+              ghLink="https://github.com/t2na/note-taking-app"
+              demoLink="https://note-creator-app-80d5bc29a5a8.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={weatherApp}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Weather Forecast"
+              description="This app allows you to check the current and forecasted weather for a given city. The application was built using the openweathermap API, and leveraged JSON objects, along with JavaScript and HTML, to neatly display the upcoming weather in your area"
+              ghLink="https://github.com/t2na/city-weather-forecast"
+              demoLink="https://t2na.github.io/city-weather-forecast/"
             />
           </Col>
         </Row>
