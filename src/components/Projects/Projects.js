cc182815d28159770f9e00cc8da30f12fe10d7cc
chinsong <1006283@mymail.sutd.edu.sg>
2023-12-06 15:06:06 +0800

    update project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,12 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import singpass from "../../Assets/Projects/singpass.png";
+import dbs from "../../Assets/Projects/dbs.jpg";
+import ocbc from "../../Assets/Projects/ocbc.jpg";
+import dnata from "../../Assets/Projects/dnata.png";
+import fronthausEventApp from "../../Assets/Projects/fronthausEventApp.png";
+import resico from "../../Assets/Projects/resico.png";
 
 function Projects() {
   return (
@@ -23,68 +29,66 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={singpass}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Spot the Differences"
+              description="The Spot the Differences game for Singpass aims to educate seniors in preventing scam applications."
+              ghLink="https://github.com/jsonggan"
+              demoLink="https://avpd.holo.sg/game/dashboard"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={dbs}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="DBS CX Day"
+              description="A series of games that engage 5,000+ users and allow them to learn more about DBS."
+              ghLink="https://github.com/jsonggan"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={ocbc}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Frank by OCBC"
+              description="A real-time updating web app that receives user card designs and displays them with elegant animations."
+              ghLink="https://github.com/jsonggan"
+              demoLink="https://frank.holo.sg/card"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={dnata}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="News Aggregator"
+              description="The Automated News Collector and Management Web App, aims to streamline the process of identifying and managing relevant news articles for dnata employees. The web application automates the retrieval of industry-specific articles from diverse sources, categorizing, summarizing, and facilitates seamless collaboration within the team."
+              ghLink="https://github.com/Service-Design-Studio/1d-final-project-2023-sds-2023-team-06"
+              demoLink="https://sites.google.com/mymail.sutd.edu.sg/sud-avengers/home"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={fronthausEventApp}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Event Application"
+              description="Designed architecture of the mobile app for event attendees so that the app can be easily customized for different events."
+              ghLink="https://github.com/jsonggan/fronthaus"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={resico}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="ResiCo"
+              description="Connecting communities together by digitalizing residential notice boards and providing an easier way for residents to talk, ask, and help each other."
+              ghLink="https://github.com/jsonggan/ResiCo"
+              demoLink="https://www.linkedin.com/feed/update/urn:li:activity:7110657925282463744/"    
             />
           </Col>
         </Row>
