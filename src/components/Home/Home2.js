90b4e0955dceb8fbf282c7f8d45066b721376995
hanshat <84309678+hanshat@users.noreply.github.com>
2023-12-26 00:13:33 +0300

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -33,7 +33,7 @@ function Home2() {
                 <b className="purple">Web Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  LiveStreaming App.
                 </b>
               </i>
               <br />
