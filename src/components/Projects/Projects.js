f1eaedc637ff98c87b3ee611d0a4bf1e86acdce3
hanshat <84309678+hanshat@users.noreply.github.com>
2023-12-26 00:16:53 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ function Projects() {
               isBlog={false}
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
+              ghLink="https://github.com/hanshat/Chatify"
               demoLink="https://chatify-49.web.app/"
             />
           </Col>
