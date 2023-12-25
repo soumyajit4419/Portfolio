6aff4881771f0160b2408139a33b02fb630547d8
hanshat <84309678+hanshat@users.noreply.github.com>
2023-12-25 23:08:10 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/hanshat"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
