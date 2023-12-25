0098c86a8c8a857472e1fdc1e03be0f706539173
hanshat <84309678+hanshat@users.noreply.github.com>
2023-12-25 22:57:34 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://iamhans.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/hanshat/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
