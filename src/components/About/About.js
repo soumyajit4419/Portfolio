0acdb6179729bcc8e197aa46516623a68d4ac25a
Jobwafula <jobwafulabg@gmail.com>
2023-12-27 21:47:48 +0300

    changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+import laptopImg from "../../Assets/portfoliopic.jpg";
 import Toolstack from "./Toolstack";
 
 function About() {
