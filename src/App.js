df9492f54ae0bf2a152d7c7e51ec4efff2a756d2
kalifkhan <85588700+kalifkhan@users.noreply.github.com>
2024-02-21 19:12:51 +0000

    Updating APP component for better performance

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
-import Preloader from "../src/components/Pre";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
+import React, { useState, useEffect, Suspense , json  } from "react";
+
+const preLoader = React.lazy(()=>import ("../src/components/Pre") );
+const Navbar = React.lazy( () =>import("./components/Navbar"));
+const  Home = React.lazy( ()=> import("./components/Home/Home"));
+const  About= React.lazy( ()=> import("./components/About/About"));
+const  Projects = React.lazy( ()=> import("./components/Projects/Projects"));
+const Footer = React.lazy( ()=> import("./components/Footer"));
+const  Resume = React.lazy( ()=> import("./components/Resume/ResumeNew"));
 import {
   BrowserRouter as Router,
   Route,
@@ -30,19 +31,23 @@ function App() {
 
   return (
     <Router>
-      <Preloader load={load} />
+      <Suspense fallback={<div>Loading...</div>}>
+      <preLoader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
+        
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
+          <Route path="/project" element={<Projects /> } />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
+        
         <Footer />
       </div>
+      </Suspense>
     </Router>
   );
 }
