d6b258a9799c681e5d5b5f62fea22dcb4f95a74f
Paulo Henriques <p.henriques@epilot.cloud>
2024-02-22 21:45:33 +0000

    Adjust home

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Software Engineer",
+          "Full Stack Developer",
         ],
         autoStart: true,
         loop: true,
