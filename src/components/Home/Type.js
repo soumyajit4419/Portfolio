dc5635154b9ffc93022b1f94819afd32664a6a65
BrandonKim922 <hwbrandonkim@gmail.com>
2024-03-27 15:04:22 -0700

    firstdraft

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,14 +6,12 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Full-Stack Software Developer"
+         
         ],
         autoStart: true,
         loop: true,
-        deleteSpeed: 50,
+        deleteSpeed: Infinity,
       }}
     />
   );
