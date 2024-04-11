83d63e4d412aa4d54f9612e9fe4dd4a81c157c91
John Lukich <john.p.lukich@gmail.com>
2024-04-11 08:25:15 -0600

    fixed particles overlapping footer

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -13,7 +13,11 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
+        <Button
+          variant="primary"
+          href={props.ghLink}
+          target="_blank"
+        >
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
