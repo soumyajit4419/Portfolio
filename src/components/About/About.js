c2932e4f251718841c42aca2fa1a8d21f7f167b6
Membathisi <membathisi.mcetywa@smokeci.com>
2024-05-27 22:04:58 +0200

    updated about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+              More about <span className="purple">me</span>
             </h1>
             <Aboutcard />
           </Col>
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,38 +1,34 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
-
+//<span className="purple">
 function AboutCard() {
   return (
     <Card className="quote-card-view">
       <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
-        </blockquote>
+      <section aria-label="About Me">
+          <h2 style={{ display: 'none' }}>About Me</h2>
+          <article className="blockquote mb-0">
+            <p style={{ textAlign: "justify" }}>
+              Hello! I am a <span className="purple">passionate</span> and driven software developer with a strong
+              background in full-stack development, focused on crafting responsive
+              and user-friendly web and mobile <span className="purple">applications</span>.
+              <br /><br />
+              My expertise spans a variety of technologies including <span className="purple">TypeScript</span>,
+              React, NestJs, MongoDB, and Docker, enabling me to build robust
+              solutions from the ground up. I thrive in dynamic environments,
+              quickly adapting to new <span className="purple">challenges</span> and continuously expanding my
+              skill <span className="purple">set</span>.
+              <br /><br />
+              Throughout my career, I have <span className="purple">effectively</span> led and
+              contributed to projects that enhance operational efficiencies and
+              improve user experiences. My <span className="purple">approach</span> is deeply rooted in a
+              commitment to quality and innovation, leveraging modern technologies
+              to solve real-world problems. As a lifelong learner, I am always
+              eager to explore new <span className="purple">technologies</span> and methodologies that unlock value for <span className="purple">people</span>.
+            </p>
+          </article>
+        </section>
       </Card.Body>
     </Card>
   );
