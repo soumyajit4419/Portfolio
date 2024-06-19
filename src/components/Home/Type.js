9f71c7fd91b58073c6ad2ec72005651d13a20dd0
nafiul-adnan <143912167+nafiul-adnan@users.noreply.github.com>
2024-06-19 22:45:29 +0100

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Programming Trainer",
+          "Career Trainer",
+          "Tech enthusiastic",
         ],
         autoStart: true,
         loop: true,
