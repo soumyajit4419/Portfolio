97db80bc074027086a0ff5b5f26afe44f146d71d
Adarsh Pakala <pakalaadarshraavan@gmail.com>
2024-07-01 11:30:38 +0530

    "Updated Link of Resume"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -85,7 +85,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/ContactForm"
+                to="/resume"
                 onClick={() => updateExpanded(false)}
               >
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
