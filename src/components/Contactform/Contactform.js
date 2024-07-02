b1422c53e18f0f04743752fd63c4f362b39a65ad
Adarsh Pakala <pakalaadarshraavan@gmail.com>
2024-07-02 21:11:37 +0530

    "Done"

--- src/components/Contactform/Contactform.js
+++ src/components/Contactform/Contactform.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { Container, Button } from "react-bootstrap";
 import emailjs from 'emailjs-com';
-import "./Contactform.css"; // Assuming you have some styles for the form
+import Button from 'react-bootstrap/Button';
+import laptopImg from "../../Assets/contanct.png";
 
-const ContactForm = () => {
+function ContactForm() {
   const initialValues = {
     name: "",
     email: "",
@@ -36,41 +37,62 @@ const ContactForm = () => {
   };
 
   return (
-    <Container className="contact-form-container">
-      <h2>Contact Us</h2>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ isSubmitting }) => (
-          <Form className="contact-form">
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <Field name="name" type="text" className="form-control" />
-              <ErrorMessage name="name" component="div" className="error-message" />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <Field name="email" type="email" className="form-control" />
-              <ErrorMessage name="email" component="div" className="error-message" />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <Field name="message" as="textarea" className="form-control" />
-              <ErrorMessage name="message" component="div" className="error-message" />
-            </div>
-
-            <Button type="submit" disabled={isSubmitting}>
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
+    <Container fluid className="contact-section">
+      <Row style={{ justifyContent: "center", padding: "10px" }}>
+        <Col
+          md={7}
+          style={{
+            justifyContent: "center",
+            paddingTop: "30px",
+            paddingBottom: "50px",
+          }}
+        >
+          <h1 style={{ fontSize: "2.1em", paddingBottom: "20px", color: "#ffffff" }}>
+            Get in <strong className="purple">Touch</strong>
+          </h1>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <Row>
+                  <Col md={6}>
+                    <label htmlFor="name" style={{ color: "#ffffff" }}>Name</label>
+                    <Field name="name" type="text" className="form-control" />
+                    <ErrorMessage name="name" component="div" className="error-message" />
+                  </Col>
+                  <Col md={6}>
+                    <label htmlFor="email" style={{ color: "#ffffff" }}>Email</label>
+                    <Field name="email" type="email" className="form-control" />
+                    <ErrorMessage name="email" component="div" className="error-message" />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={12}>
+                    <label htmlFor="message" style={{ color: "#ffffff" }}>Message</label>
+                    <Field name="message" as="textarea" className="form-control" />
+                    <ErrorMessage name="message" component="div" className="error-message" />
+                  </Col>
+                </Row>
+                <Button type="submit" disabled={isSubmitting}>
+                  Submit
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+        <Col
+          md={5}
+          style={{ paddingTop: "120px", paddingBottom: "50px" }}
+          className="contact-img"
+        >
+          <img src={laptopImg} alt="contact" className="img-fluid" />
+        </Col>
+      </Row>
     </Container>
   );
-};
+}
 
 export default ContactForm;
