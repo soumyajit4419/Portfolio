585e91a98bed91ff169614078c04f2bcc04bc3c6
ZDNCH <muhammadzaidanfaiz8@gmail.com>
2024-07-05 19:38:17 +0700

    mas

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,7 +21,7 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-        {/* <Col md={4} className="project-card">
+       <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
@@ -30,7 +30,7 @@ function Projects() {
               ghLink="https://github.com/soumyajit4419/Chatify"
               demoLink="https://chatify-49.web.app/"
             />
-          </Col> */}
+          </Col> 
         
         </Row>
       </Container>
