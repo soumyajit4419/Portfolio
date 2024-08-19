7e351cfa5102b51b4069ce9d59ffe6679e6d358b
Rohal Jamal <rohaljamal12345@gmail.com>
2024-08-19 20:05:25 +0000

    updated the type.js file.#

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,7 +7,7 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
+          "Data Analyst",
           "MERN Stack Developer",
           "Open Source Contributor",
         ],
