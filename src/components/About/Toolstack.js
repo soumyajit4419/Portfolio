aa885cc85514d35584e655ea536a9e1f3e5460db
Adarsh Pakala <pakalaadarshraavan@gmail.com>
2024-09-12 09:38:34 +0530

    "Done with changes"

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiMacos,
+  SiWindows,
+  SiDigitalocean,
+  SiDocker,
+  SiWails,
+  SiAmazonaws,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDigitalocean />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazonaws />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
