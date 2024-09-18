806e104fc1c16882168f7540a6e9e6bda0c402f1
Hector Barahona <hbarahona022@gmail.com>
2024-09-18 13:03:08 -0500

    new home img

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import homeLogo from "../../Assets/home-main.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
