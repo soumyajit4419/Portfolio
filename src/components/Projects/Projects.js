a337e25e676e1f5e5b38e6d0e3f640d039379a8e
Ali El-esawy <73000699+alielesawy@users.noreply.github.com>
2024-09-22 21:46:02 +0000

    scond customization

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/Chat Room (1).png";
 import editor from "../../Assets/Projects/Chat Room (3).png";
 import chatify from "../../Assets/Projects/Chat Room (2).png";
@@ -24,10 +23,9 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="RoomChat"
+              description="The JavaFX Chat Application is a desktop application that facilitates real-time communication between multiple users over a local server. Built using JavaFX for the user interface and Java Sockets for network communication, this application serves as a robust platform for chatting and managing user connections in a local network environment."
+              ghLink="https://github.com/alielesawy/chat-group"
             />
           </Col>
 
@@ -37,10 +35,9 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="ATM"
+              description="JAVA-IMPLEMENTED SIMULATION FOR ATM MACHINE USING MYSQL"
+              ghLink="https://github.com/alielesawy/ATM-"
             />
           </Col>
 
@@ -48,9 +45,9 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Paint App"
+              description="JAVA AND JAVAFX FOR PAINT APP."
+              //ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
@@ -59,10 +56,9 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="PC Age"
+              description="APPLICATION TO DETERMINE THE AGE OF A pc AND RETRIEVE THE MANUFACTURING DATE FROM THE BIOS."
+              ghLink="https://github.com/alielesawy/Your-PC-Age"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
