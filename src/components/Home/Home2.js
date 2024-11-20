1c9ad4ea957b552fcc60f9c5ddf7b97d328693b6
Agraj Agrawal <agrajagrawal4@gmail.com>
2024-11-20 08:50:27 +0530

    add: home page modification

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,38 +19,15 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              With over two years of experience in full-stack development, I specialize in backend engineering, particularly in <b className="purple">payments</b> and <b className="purple">FinTech</b>.
+              <br /><br />
+              I’m proficient in <i><b className="purple">Node.js</b></i>, <i><b className="purple">React.js</b></i>, <i><b className="purple">Go</b></i>, and <i><b className="purple">Apache Kafka</b></i>.
+              <br /><br />
+              I’m always exploring and learning, especially in <i><b className="purple">data analysis</b></i>, <i><b className="purple">stock market analysis</b></i>, and personal development.
+              <br /><br />
+              My focus is on building scalable solutions using <i><b className="purple">AWS</b></i>, <i><b className="purple">MongoDB</b></i>, and <i><b className="purple">TypeScript</b></i>, while continuously working on improving myself.
             </p>
+
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
@@ -75,7 +52,7 @@ function Home2() {
                   <AiFillGithub />
                 </a>
               </li>
-              <li className="social-icons">
+              {/* <li className="social-icons">
                 <a
                   href="https://twitter.com/Soumyajit4419"
                   target="_blank"
@@ -84,10 +61,10 @@ function Home2() {
                 >
                   <AiOutlineTwitter />
                 </a>
-              </li>
+              </li> */}
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/agrajagrawal/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +74,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/agraj_agrawal/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Software Engineer",
+          "Finance Enthusiast",
+          "Software Engineer",
+          "Competitive Programmer",
         ],
         autoStart: true,
         loop: true,
