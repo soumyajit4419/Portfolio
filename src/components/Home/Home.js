507d6d763302024625f2bea111a18c8e7a2b22ba
Josue Soto <sotoherrerajosue@gmail.com>
2024-11-27 22:27:52 -0600

    set information of Josue

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
+                Hi!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> Josué Soto</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,11 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Software Engineer",
+          "Web3 Developer",
+          "Dojo Coding Community",
+          "Open Source Contributor / OnlyDust",
+          "Backend / Frontend",
         ],
         autoStart: true,
         loop: true,
