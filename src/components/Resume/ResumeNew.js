28f9fb9c33ced93650f7ec2a4a2a02aceed1bd8b
Ali El-esawy <73000699+alielesawy@users.noreply.github.com>
2024-12-21 16:15:42 +0000

    some updates

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/resume 20-9-2024.pdf";
+import pdf from "../../Assets/../Assets/Ali Esmaeil - DevOps Enginner.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
