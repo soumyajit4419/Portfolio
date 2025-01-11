1d33528f2047e6cb5390aae9ecedcc6086a45da9
Hafeefa Sultan <99234946+hafeefas@users.noreply.github.com>
2025-01-11 02:12:44 +0000

    changed up footer to add my links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,7 +11,8 @@ function Contact() {
         <h1 className="project-heading">
           <strong className="purple">Contact Me</strong>
         </h1>
-        <p style={{ color: "white" }}>Here's how you can reach me</p>
+        {/* <p style={{ color: "white" }}>Here's how you can reach me</p> */}
+        <p style={{ color: "white" }}>Whoops! This page is under construction. For now, contact me via LinkedIn!</p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ContactCard
