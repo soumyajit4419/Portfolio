03fc587d079ed31bf816a347d2b5783f6b9dcd73
unknown <sacha.desquesnes@gmail.com>
2025-04-04 22:33:42 +0200

    Modification fonctionnement et style About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,52 +5,45 @@ import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
+import About2 from "./About2";
 
 function About() {
   return (
-    <Container fluid className="about-section">
-      <Container >
-        <div className="about-header">
+    <section>
+      <Container fluid className="about-section">
+        <Container >
+          <div className="about-header">
 
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
-          <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Qui <strong className="purple">suis-je </strong>?
-            </h1>
-            <Aboutcard />
-          </Col>
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img">
-            <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>
-        </Row>
-        </div>
-       
-        <div className="about-footer">
-
-          <h1 className="project-heading">
-            <strong className="purple">Connaissances</strong> techniques
-          </h1>
-
-          <Techstack />
-
-          <h1 className="project-heading">
-            <strong className="purple">Outils</strong>
-          </h1>
-          <Toolstack />
-        </div>
-            
+          <Row style={{ justifyContent: "center", padding: "10px" }}>
+            <Col
+              md={7}
+              style={{
+                justifyContent: "center",
+                paddingTop: "30px",
+                paddingBottom: "50px",
+              }}
+            >
+              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+                Qui <strong className="purple">suis-je </strong>?
+              </h1>
+              <Aboutcard />
+            </Col>
+            <Col
+              md={5}
+              style={{ paddingTop: "120px", paddingBottom: "50px" }}
+              className="about-img">
+              <img src={laptopImg} alt="about" className="img-fluid" />
+            </Col>
+          </Row>
+          </div>
+        
+          
+              
+        </Container>
       </Container>
-    </Container>
+
+      <About2 />
+    </section>
   );
 }
 
---
+++ src/components/About/About2.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Github from "./Github";
+import Techstack from "./Techstack";
+import Aboutcard from "./AboutCard";
+import laptopImg from "../../Assets/about.png";
+import Toolstack from "./Toolstack";
+
+function About2() {
+    return (
+        <Container fluid className="about-section-footer">
+            <Container >
+                <div className="about-footer">
+
+                    <h1 className="project-heading">
+                        <strong className="home-text">Connaissances</strong> techniques
+                    </h1>
+                    <Techstack />
+
+                    <h1 className="project-heading">
+                        <strong className="home-text">Outils</strong>
+                    </h1>
+                    <Toolstack />
+                
+                </div>
+            </Container>
+        </Container>
+    );
+}
+export default About2;
