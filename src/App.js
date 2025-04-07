c77c6b15dcb2eb591e2bc72041f6506fd64d1db2
unknown <sacha.desquesnes@gmail.com>
2025-04-07 17:44:41 +0200

    Analytics viré

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import AnimatedSquares from "./components/AnimatedSquares";
-import { Analytics } from "@vercel/analytics/react";
 import {
   BrowserRouter as Router,
   Route,
@@ -47,7 +46,6 @@ function App() {
           {/*<Route path="*" element={<Error404 />} />*/}ppm
         </Routes>
         <Footer />
-        <Analytics />
       </div>
       
     </Router>
