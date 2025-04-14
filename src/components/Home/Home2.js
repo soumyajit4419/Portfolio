f8856cbb504eaccbdd349aa0fb43bd823778e19d
sazzadswe36 <sazzadnayem.swe@gmail.com>
2025-04-15 04:05:35 +0600

    Pic added in svg to PNG

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avatar.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
