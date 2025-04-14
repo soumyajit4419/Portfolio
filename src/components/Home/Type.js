405bcda63e2a866b142c0bb1ea3ba92ce9dda8a9
sazzadswe36 <sazzadnayem.swe@gmail.com>
2025-04-15 03:26:43 +0600

    Added a Type section SWE

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,9 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
+          "Software Engineer",
+          "Front-end Developer",
           "Open Source Contributor",
         ],
         autoStart: true,
